7196672b3f840bfef43df95d0feff348f7526061
Samukelo khathi <145541253+Samukelokhathi@users.noreply.github.com>
2026-05-27 14:19:09 +0200

    updated conditionalStatement_loops.js

--- conditionalStatements_loops.js
+++ conditionalStatements_loops.js
@@ -18,7 +18,7 @@ if (temperature < 0) {
 
 let divisibleBy3 = 3;
 let divisibleBy2 = 2;
-let number = 7;
+let number = 9;
 
 if (number % divisibleBy2 === 0 && number % divisibleBy3 === 0) {
     console.log("Divisible by both.")
@@ -30,38 +30,90 @@ if (number % divisibleBy2 === 0 && number % divisibleBy3 === 0) {
     console.log("Not divisible by 2 and 3")
 }
 
-// For Loop 
+switch (true) {
+    case number % divisibleBy2 === 0 && number % divisibleBy3 === 0:
+        console.log("Divisible by both.")
+        break;
 
-// numbers from 1 - 10
+    case number % divisibleBy2 === 0:
+        console.log("Divisible by 2")
+        break;
+
+    case number % divisibleBy3 === 0:
+        console.log("Divisibile by 3")
+        break;
+
+    default:
+        console.log("Not divisible by 2 and 3")
+        break;
+
+}
+
+// For Loops
+
+// 1. Printing numbers from 1 - 10
 for (let num = 1; num <= 10; num++) {
     // console.log(num)
 }
 
+// 2. Even number from 1 - 20 
 for (let num = 1; num <= 20; num++) {
     if (num % 2 === 0) {
         // console.log(num)
     }
 }
 
-// Each 
+// 3. Sum of all numbers from 1 to 100
 let sum = 0;
 for (let num = 1; num <= 100; num++) {
     sum += num
 }
-// console.log(sum)
+// console.log('Sum:', sum)
 
 
-// Loops with array
+// 4. Loops with array
 
 const numbers = [1, 2, 3, 4, 5];
-for (let num = 1; num <= numbers.length; num++) {
-    // console.log(num)
+for (let num = 0; num < numbers.length; num++) {
+    // console.log(numbers[num])
 }
 
+// 5. Printing Large Numbers
 const numbersArr = [3, 7, 2, 5, 10, 6]
-let currentValue;
-let prevValue;
+let largestNumber = 0;
 
 for (let num = 0; num < numbersArr.length; num++) {
-    console.log(num)
+    if (numbersArr[num] > largestNumber) {
+        largestNumber = numbersArr[num]
+    }
 }
+// console.log(largestNumber)
+
+
+// While loop
+
+
+count = 1
+
+while (count <= 10) {
+    console.log(count)
+    count++
+}
+
+
+
+
+
+// const guessNumber = 10
+// let userInput
+
+// do
+// {
+//     userInput = parseInt(prompt("Print guess a number between 1 to 10 "))
+
+//     if(userInput === guessNumber){
+//         console.log("The guess is true")
+//     }
+// }while(true)
+
+
